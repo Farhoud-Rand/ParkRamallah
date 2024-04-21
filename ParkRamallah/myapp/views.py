8e93900c2909df9aa154180c54a096046b99bcaf
Rand <r.farhoud2000@gmail.com>
2024-04-22 02:17:05 +0300

    Users: login page first version

--- ParkRamallah/myapp/views.py
+++ ParkRamallah/myapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Users: login page 
 def login(request):
     return render(request,"login.html")
+
+# Users: Register page 
+def register(request):
+    return render(request,"register.html")
